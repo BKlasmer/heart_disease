cd3a88db7619a2ee6828265877f2b6e6082c2322
Ben Klasmer <benjamin_aram@hotmail.com>
2021-02-20 11:08:29 +0000

    Initial commit

---
+++ heart_disease/data_loader.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: latin-1 -*-
+
+""" Class to ingest UCI Heart Disease Data Set - https://archive.ics.uci.edu/ml/datasets/Heart+Disease
+"""
+
+class DataLoader(object):
+    def __init__(self):
+        pass
